8ff1f61e57555c898eb719cd9d1291cbc6757002
BlackwaveZero <61608293+BlackwaveZero@users.noreply.github.com>
2020-05-25 23:01:50 +0430

    Create removeWhiteSpace.py

---
+++ removeWhiteSpace.py
@@ -0,0 +1,29 @@
+def isWhite(ch):
+    if ch == ' ' or ch == '\t':
+        return True
+    return False
+
+def removeWhiteSpaces(text):
+    if len(text) == 0:
+        return ''
+    tmp = ''
+    flag = isWhite(text[0])
+    for ch in text:
+        if flag:
+            if not isWhite(ch):
+                tmp+=ch
+                flag=False
+        else:
+            if isWhite(ch):
+                tmp+=' '
+                flag=True
+            else:
+                tmp+=ch
+    if flag:
+        return tmp[0:len(tmp)-1]
+    return tmp
+data=input()
+if(len(data))!=0:
+    print(removeWhiteSpaces(data))
+# def run(text):
+#     return [removeWhiteSpaces(text)]
